69b5cb4b4a18e5cd57bc1b067fcf1c040dbad3f8
Spill-Tea <delrioj17@gmail.com>
2025-07-12 16:37:59 -0700

    feat(docs._ext): Update extensions with new updates to PyTemplate for custom syntax highlighting for code snippets.

--- docs/source/_ext/styles.py
+++ docs/source/_ext/styles.py
@@ -27,7 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Custom Pygment styles."""
+"""Custom Pygment syntax highlighting style."""
 
 from typing import ClassVar
 
@@ -39,43 +39,107 @@ from pygments.token import (
     Name,
     Number,
     Operator,
+    Other,
     Punctuation,
     String,
+    Text,
     _TokenType,
 )
+from utils import get_brackets
+
+
+def bold(color: str) -> str:
+    """Embolden color."""
+    return f"bold {color}"
+
+
+def italic(color: str) -> str:
+    """Italicize color."""
+    return f"italic {color}"
+
+
+def underline(color: str) -> str:
+    """Underline text with color."""
+    return f"underline {color}"
+
+
+class Colors:
+    """Define colors used more than once."""
+
+    datatype: str = "#61C8B0"
+    variable: str = "#9CDCFE"
+    function: str = "#DCDCAA"
+    reserved: str = "#639BD4"
+    default: str = "#D4D4D4"
+    control: str = "#C586C0"
+    builtin: str = "#4EC9B0"
+    declare: str = "#569CD6"
+    problem: str = "#C3726A"
+    comment: str = "#6A9955"
+    bracket: str = "#F9C922"
 
 
 class VSCodeDarkPlus(Style):
-    """VSCode Dark+ Style."""
+    """Custom theme deeply inspired by VSCode Dark+ as a pygments style."""
 
     background_color: str = "#1E1E1E"
 
-    styles: ClassVar[dict[_TokenType, str]] = {
-        Number: "#B6CEA9",
-        Operator: "#D4D4D4",
-        Operator.Word: "#C586C0",
-        Comment: "#6D9957",
-        Comment.Preproc: "#639BD4",
-        Keyword.Namespace: "#C287A0",
-        # Keyword.Reserved: "#C287A0",
-        Keyword.Reserved: "#639BD4",
-        Keyword.Type: "#61C8B0",
-        Keyword.Constant: "#4FC1FF",
-        # Keyword: "#639BD4",
-        Keyword: "#C586C0",
-        Name: "#7FD0FD",
-        Name.Class: "#61C8B0",
-        Name.Namespace: "#61C8B0",
-        Name.Function: "#DCDCAA",
-        # Name.Builtin: "#DCDCAA",
-        Name.Builtin: "#4EC9B0",
-        Name.Type: "#4EC9B0",
-        Name.Builtin.Pseudo: "#9CDCFE",
-        Name.Variable: "#9CDCFE",
-        Name.Variable.Class: "#61C8B0",
-        Name.Variable.Magic: "#DCDCAA",
-        Name.Exception: "#61C8B0",
-        Error: "#61C8B0",
+    styles: ClassVar[dict[_TokenType, str]] = {  # pyright: ignore
+        # Comments
+        Comment: Colors.comment,
+        Comment.Single: Colors.comment,
+        Comment.Preproc: Colors.reserved,
+        Comment.Special: bold(Colors.declare),
+        Comment.Hashbang: italic("#7C7046"),
+        Comment.Multiline: italic("#525252"),
+        # Keywords
+        Keyword: Colors.control,
+        Keyword.Type: Colors.datatype,
+        Keyword.Declare: bold(Colors.declare),
+        Keyword.Constant: bold(Colors.declare),
+        Keyword.Reserved: bold(Colors.reserved),
+        Keyword.Namespace: Colors.control,
+        # Variable Names
+        Name: Colors.variable,
+        Name.Type: Colors.builtin,
+        Name.Class: bold(Colors.datatype),
+        Name.Builtin: Colors.builtin,
+        Name.Builtin.Pseudo: italic(Colors.variable),
+        Name.Constant: "#4FC1FF",
+        Name.Function: Colors.function,
+        Name.Function.Magic: italic(Colors.function),
+        Name.Variable: Colors.variable,
+        Name.Variable.Class: Colors.datatype,
+        Name.Variable.Magic: Colors.function,
+        Name.Namespace: Colors.datatype,
+        Name.Exception: Colors.problem,
+        # (Doc)Strings
+        Text: Colors.default,
         String: "#C9937A",
-        Punctuation: "#F9C922",
+        String.Doc: italic(Colors.comment),
+        String.Doc.Title: bold("#80AE6B"),
+        String.Affix: Colors.declare,
+        String.Regex: "#D16969",
+        String.Escape: "#D7BA7D",
+        String.Interpol: Colors.declare,
+        # Numbers
+        Number: "#B6CEA9",
+        Number.Other: Colors.declare,
+        # Operators
+        Operator: Colors.default,
+        Operator.Word: Colors.control,
+        # Punctuation
+        Punctuation: Colors.default,
+        **get_brackets(
+            [
+                Colors.bracket,
+                "#EA2EEA",
+                "#5DCD4C",
+                "#3B9ADE",
+            ]
+        ),
+        Punctuation.Error: underline("#F92222"),
+        # Miscellaneous
+        Error: underline(bold(Colors.problem)),
+        Other: Colors.default,
     }
